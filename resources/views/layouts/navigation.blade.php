3545355891b0876faf28b35d2850d53617650635
Takayuki Nakatsuji <129576342+takayuki91@users.noreply.github.com>
2023-07-29 18:37:12 +0900

    [Add]Add-logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,8 +5,8 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    <a href="{{ route('post.index') }}">
+                        <x-application-logo width="50" class="block text-gray-800" />
                     </a>
                 </div>
 
