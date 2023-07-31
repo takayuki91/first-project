3b66f8bae6b1336682fa5a52e42523a8c962463c
Takayuki Nakatsuji <129576342+takayuki91@users.noreply.github.com>
2023-07-31 14:02:20 +0900

    [Fix]csvfile-download

--- resources/views/post/download_csv.blade.php
+++ resources/views/post/download_csv.blade.php
@@ -1,11 +1,19 @@
 <x-app-layout>
   <x-slot name="header">
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-          CSVファイルをダウンロード
+          時短飯をダウンロード
       </h2>
   </x-slot>
 
-  <div class="max-w-7xl mx-auto px-6">
-    <a href="{{ route('post.download') }}">CSVファイルをダウンロード</a>
+  <div class="max-w-7xl mx-auto px-6 mt-4">
+      <div class="text-center">
+        <h1 class="text-lg font-semibold mb-4">
+          全ての時短飯をダウンロードできます🍚<br>（形式:CSVファイル）
+        </h1>
+        <hr class="w-full mb-4">
+          <a href="{{ route('post.download_csv_file') }}" class="download-btn">
+              ダウンロード
+          </a>
+      </div>
   </div>
 </x-app-layout>
