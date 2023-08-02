96a4ea04fe120ec4641e09a04297e3fbe0079fad
Takayuki Nakatsuji <129576342+takayuki91@users.noreply.github.com>
2023-08-02 12:42:09 +0900

    [Add]Posting images

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,7 +12,8 @@ class Post extends Model
     protected $fillable = [
         'title',
         'body',
-        'user_id'
+        'user_id',
+        'image'
     ];
 
     // Userモデルとのリレーション
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -15,7 +15,7 @@
     {{-- コンポーネントに変更 --}}
     <x-message :message="session('message')" />
 
-    <form method="post" action="{{ route('post.update', $post) }}">
+    <form method="post" action="{{ route('post.update', $post) }}" enctype="multipart/form-data">
       @csrf
       @method('patch')
       <div class="mt-8">
@@ -30,6 +30,19 @@
         <label for="body" class="font-semibold mt-4">本文</label>
         <x-input-error :messages="$errors->get('body')" class="mt-2" />
         <textarea name="body" class="w-auto py-2 border border-gray-300 rounded-md" id="body" cols="30" rows="5">{{old('body', $post->body)}}</textarea>
+      </div>
+
+      <div class="w-full flex flex-col">
+        @if($post->image)
+          <img src="{{ asset('storage/images/'.$post->image)}}" class="mx-auto" style="height:300px;">
+          <div>
+            (画像ファイル：{{$post->image}})
+          </div>
+        @endif
+        <label for="image" class="font-semibold leading-none mt-4">画像（1MBまで）</label>
+        <div>
+            <input id="image" type="file" name="image">
+        </div>
       </div>
 
       <x-primary-button class="mt-4">
